a2a2d0c01e1d2c6eda71150373e8f34da88b985b
07santiago21 <santiagolopezramirez0721@gmail.com>
2025-04-15 22:36:09 -0500

    cat resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 
-import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProjectsModule } from './projects/projects.module';
 
 
 
@@ -24,15 +22,12 @@ import { ProjectsModule } from './projects/projects.module';
       username: 'santiago',
       password: 'santiago',
       database: 'ejemplo',
-      entities: ['dist/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
       synchronize: true,
       retryDelay:3000,
       retryAttempts:10
     }),
 
-    UsersModule,
-
-    ProjectsModule
   ],
 })
 export class AppModule {}
